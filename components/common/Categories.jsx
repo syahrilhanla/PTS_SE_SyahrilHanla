fa7f1bc96529feda2dbdd8275813d9317996ebeb
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-20 01:34:40 +0800

    refactor category button to button component

--- components/common/Categories.jsx
+++ components/common/Categories.jsx
@@ -4,18 +4,31 @@ import { GiTopHat } from "react-icons/gi";
 const Categories = () => {
 	const CardCategory = ({ category }) => {
 		return (
-			<div class="text-slate-200 w-36 p-2 max-w-sm bg-[#729ff1] rounded-lg flex justify-center items-center gap-4">
-				<BiShoppingBag className="w-8 h-8 text-slate-300" />
+		
+
+				<div className="w-full">
+				<button
+					type="button"
+					class="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4
+					 focus:ring-blue-200 font-medium rounded-lg gap-4
+					 text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
+					  focus:outline-none dark:focus:ring-blue-800 flex justify-center items-center"
+				>
+				<BiShoppingBag className="w-8 h-8 text-slate-100" />
 				<div className="flex flex-col justify-center items-center">
 					<h2 className="">{category}</h2>
 					<p className="font-light text-xs">64 items</p>
 				</div>
+				</button>
 			</div>
 		);
 	};
 
 	return (
-		<div className="md:w-full w-fit mx-auto md:flex px-4 grid grid-cols-2 gap-4 justify-center items-center">
+		<div
+			className="md:w-full w-fit mx-auto md:flex px-4 grid grid-cols-2
+		gap-4 justify-center items-center"
+		>
 			<CardCategory category={"All Item"} />
 			<CardCategory category={"Hats"} />
 			<CardCategory category={"Tops"} />
