06b484dda703610f1730672c194e0c86cd743696
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-20 08:48:17 +0800

    refactor class to className

--- components/common/Categories.jsx
+++ components/common/Categories.jsx
@@ -1,24 +1,21 @@
 import { BiShoppingBag } from "react-icons/bi";
-import { GiTopHat } from "react-icons/gi";
 
 const Categories = () => {
 	const CardCategory = ({ category }) => {
 		return (
-		
-
-				<div className="w-full">
+			<div className="w-full">
 				<button
 					type="button"
-					class="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4
+					className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4
 					 focus:ring-blue-200 font-medium rounded-lg gap-4
 					 text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
 					  focus:outline-none dark:focus:ring-blue-800 flex justify-center items-center"
 				>
-				<BiShoppingBag className="w-8 h-8 text-slate-100" />
-				<div className="flex flex-col justify-center items-center">
-					<h2 className="">{category}</h2>
-					<p className="font-light text-xs">64 items</p>
-				</div>
+					<BiShoppingBag className="w-8 h-8 text-slate-100" />
+					<div className="flex flex-col justify-center items-center">
+						<h2 className="">{category}</h2>
+						<p className="font-light text-xs">64 items</p>
+					</div>
 				</button>
 			</div>
 		);
--- components/transaction/Cashier.jsx
+++ components/transaction/Cashier.jsx
@@ -37,7 +37,7 @@ const Cashier = () => {
 			<div className="w-full">
 				<button
 					type="button"
-					class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+					className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
 					 focus:ring-blue-300 font-medium rounded-lg gap-4
 					 text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
 					  focus:outline-none dark:focus:ring-blue-800 flex justify-center items-center"
