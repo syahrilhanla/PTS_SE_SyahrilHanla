1682bc6d0ccaa4b38ec494c9b6597e76773b372b
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-20 21:58:54 +0800

    debugged sidebar active button

--- components/common/Sidebar.jsx
+++ components/common/Sidebar.jsx
@@ -1,13 +1,13 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 import { GrTransaction } from "react-icons/gr";
 import { HiOutlineDocumentReport } from "react-icons/hi";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { GiIsland } from "react-icons/gi";
-import { useState } from "react";
 
 const Sidebar = () => {
-	const [option, setOption] = useState(1);
+	const router = useRouter();
 
 	const navButtons = [
 		{
@@ -27,18 +27,17 @@ const Sidebar = () => {
 		},
 	];
 
-	const Navigations = ({ navButton, option, index }) => {
+	const Navigations = ({ navButton }) => {
 		return (
 			<li>
 				<Link href={navButton.link}>
 					<a
 						className={`${
-							option === index + 1 && "bg-gray-100"
+							router.pathname === navButton.link && "bg-gray-100"
 						} flex items-center md:justify-start 
 					justify-center p-2 text-base font-normal 
 					text-gray-900 rounded-lg dark:text-white
 					 hover:bg-gray-200 dark:hover:bg-gray-500`}
-						onClick={() => setOption(index + 1)}
 					>
 						{navButton.icon()}
 						<span className="ml-3 md:block hidden">{navButton.text}</span>
@@ -66,12 +65,8 @@ const Sidebar = () => {
 
 					{/* Navigations */}
 					<ul className="space-y-6 md:space-y-3 px-3 flex flex-col justify-center">
-						{navButtons.map((navButton, index) => (
-							<Navigations
-								navButton={navButton}
-								index={index}
-								option={option}
-							/>
+						{navButtons.map((navButton) => (
+							<Navigations key={navButton.text} navButton={navButton} />
 						))}
 					</ul>
 				</div>
