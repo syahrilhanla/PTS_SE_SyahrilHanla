17432db162650c69133ca40794790ff3d8bc23f2
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-22 20:40:42 +0800

    display best selling item card info

--- components/summary/BestSelling.jsx
+++ components/summary/BestSelling.jsx
@@ -4,21 +4,35 @@ import BestCategoryCard from "./BestCategoryCard";
 import BestItemCard from "./BestItemCard";
 
 const BestSelling = () => {
-	const { summaryData } = useContext(GlobalContext);
+	const { summaryData, itemList } = useContext(GlobalContext);
+
+	const bestItem = itemList.filter(
+		(item) => item.name === summaryData.bestSellingItem.itemName
+	)[0];
 
 	return (
 		<div className="mt-6">
 			<h3 className="font-medium text-2xl mb-1">Best Selling Item</h3>
 			<hr className="mb-3" />
-			<span className="flex w-full justify-center">
-				<BestItemCard />
-			</span>
 
-			<h3 className="font-medium text-2xl mb-1 mt-6">Best Selling Category</h3>
-			<hr className="mb-3" />
-			<span className="flex w-full justify-center">
-				<BestCategoryCard bestCategory={summaryData.bestSellingCategory} />
-			</span>
+			{itemList.length > 0 && (
+				<>
+					{" "}
+					<span className="flex w-full justify-center">
+						<BestItemCard
+							bestItem={bestItem}
+							revenue={summaryData.bestSellingItem.revenue}
+						/>
+					</span>
+					<h3 className="font-medium text-2xl mb-1 mt-6">
+						Best Selling Category
+					</h3>
+					<hr className="mb-3" />
+					<span className="flex w-full justify-center">
+						<BestCategoryCard bestCategory={summaryData.bestSellingCategory} />
+					</span>
+				</>
+			)}
 		</div>
 	);
 };
