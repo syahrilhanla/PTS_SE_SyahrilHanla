cc68d5d5a9ccd9d62bb9f31b4db285023e25a5bd
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-22 15:25:23 +0800

    changed file name

---
+++ components/utils/useProcessSummary.js
@@ -0,0 +1,31 @@
+import { fetchTransaction } from "./dataFetchers";
+import { getBestSellingItem } from "./getBestSellings";
+
+const useProcessSummary = async () => {
+	const response = await fetchTransaction();
+	const transactionData = response.filter(
+		(data) => Array.isArray(data) === true
+	);
+
+	const { bestSellingItem, bestSellingCategory, revenuePerCategory } =
+		getBestSellingItem(transactionData);
+
+	const totalTransaction = transactionData.length;
+	const totalRevenue = revenuePerCategory
+		.map((category) => category.revenue)
+		.reduce((a, b) => a + b, 0);
+
+	const summaryObject = {
+		totalTransaction,
+		totalRevenue,
+		bestSellingCategory,
+		bestSellingItem,
+		revenuePerCategory,
+	};
+
+	console.log(summaryObject);
+
+	return { transactionData, summaryObject };
+};
+
+export default useProcessSummary;
