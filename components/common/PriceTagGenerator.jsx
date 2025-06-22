d4352cab769379e7650dd6c4791be65a129e54b3
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-22 10:29:54 +0700

    feat: enhance PriceTagGenerator with formatted price display and improve ProductCard layout

--- components/common/PriceTagGenerator.jsx
+++ components/common/PriceTagGenerator.jsx
@@ -1,11 +1,14 @@
+import { formatPriceIDR } from "../helpers/formatPriceIDR";
+
 const PriceTagGenerator = ({ category, item }) => {
 	if (item.prices.filter((price) => price.priceFor === category).length > 0) {
 		return (
-			<p className="flex justify-between w-full">
-				<span>{category}</span>
+			<p className="flex justify-between w-full text-slate-700">
+				<span className="capitalize">{category}</span>
 				<span>
-					Rp.{" "}
-					{item.prices.filter((price) => price.priceFor === category)[0].price}
+					{formatPriceIDR(
+						item.prices.filter((price) => price.priceFor === category)[0].price
+					)}
 				</span>
 			</p>
 		);
--- components/transaction/ProductCard.jsx
+++ components/transaction/ProductCard.jsx
@@ -8,21 +8,20 @@ const ProductCard = ({ item }) => {
 		useContext(GlobalContext);
 
 	return (
-		<div className="w-full bg-white rounded-lg shadow-md flex flex-col justify-center dark:bg-gray-800 dark:border-gray-700">
+		<div className="w-full py-3 bg-white rounded-lg shadow-md flex flex-col justify-center dark:bg-gray-800 dark:border-gray-700">
 			<Image
 				src={item.image}
 				alt={item.name}
 				className="object-contain p-8 rounded-t-lg md:h-56 h-fit"
-				width={200}
-				height={200}
+				width={280}
+				height={280}
+				quality={100}
 			/>
 
-			<div className="px-5 pb-5">
-				<a>
-					<h5 className="text-lg text-center font-normal tracking-tight text-gray-900 dark:text-white">
-						{item.name}
-					</h5>
-				</a>
+			<div className="px-5 pb-5 mt-3">
+				<h5 className="text-sm md:text-base text-center tracking-tight text-gray-900 dark:text-white">
+					{item.name}
+				</h5>
 
 				<div className="flex flex-col justify-between items-center mt-3">
 					<div className="text-2xl w-full h-14 mb-3">
